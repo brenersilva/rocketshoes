5ebe8b7eba9f3186e91c5bb5c72619348776ceab
Brener Silva <rib.silva@outlook.com>
2019-12-26 18:04:08 -0300

    Home stylization.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,112 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Lebron</strong>
+        <span>R$349,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
