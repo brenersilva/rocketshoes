ce50eb9dabadb7bb20508a25bc6f332cc65d28ef
Brener Silva <rib.silva@outlook.com>
2019-12-26 20:29:47 -0800

    Searching for products in API.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,112 +1,48 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  render() {
+    const { products } = this.state;
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-lebron-witness-iv-masculino/26/HZM-2140-026/HZM-2140-026_zoom1.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis Nike Lebron</strong>
-        <span>R$349,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
